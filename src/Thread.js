314f01760c97495b7f8fbbace4b27b35967e7c21
Alec Savvy <alecjsavvy@gmail.com>
2023-02-20 08:26:41 -0700

    rendering thread

--- src/Thread.js
+++ src/Thread.js
@@ -2,5 +2,17 @@ import { useSelector, useDispatch } from "react-redux";
 
 export default () => {
   const dispatch = useDispatch();
-  return <div className="thread"></div>;
+  const {
+    post: {
+      data: { children },
+    },
+    replies,
+  } = useSelector((state) => state.thread.value);
+  const thread = children[0].data;
+  console.log(thread);
+  return (
+    <div className="thread">
+      <h1>{thread.title}</h1>
+    </div>
+  );
 };
