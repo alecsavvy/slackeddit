018d094c885cbebd08e5156a0af7dd6e596aa3f1
Alec Savvy <alecjsavvy@gmail.com>
2023-02-20 07:53:56 -0700

    remove counter

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,5 @@
 import { useSelector } from "react-redux";
 
 export default () => {
-  const count = useSelector((state) => state.counter.value);
-  return (
-    <div className="sidebar">
-      Sidebar!
-      <span>{count}</span>
-    </div>
-  );
+  return <div className="sidebar">Sidebar!</div>;
 };
--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
 import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counterSlice";
 import pageReducer from "./pageSlice";
 import feedReducer, { fetchFrontPageThunk } from "./feedSlice";
 import thunk from "redux-thunk";
@@ -7,7 +6,6 @@ import thunk from "redux-thunk";
 export default configureStore(
   {
     reducer: {
-      counter: counterReducer,
       feed: feedReducer,
       page: pageReducer,
     },
