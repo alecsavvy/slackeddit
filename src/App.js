e29e08df15092cc605a93a04909132c355c194d6
Alec Savvy <alecjsavvy@gmail.com>
2023-02-20 08:19:40 -0700

    set thread

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useSelector } from "react-redux";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Thread from "./Thread";
+import { isEmpty } from "lodash";
 
 export default () => {
+  const hasThread = useSelector((state) => !isEmpty(state.thread.value.post));
   return (
     <div className="container">
       <Sidebar />
       <Feed />
+      {hasThread && <Thread />}
     </div>
   );
 };
